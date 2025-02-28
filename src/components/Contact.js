6bd6c4bdf9009b7e3370142176d775efa2f12215
leewenming17160 <leewenming17160@gmail.com>
2025-02-28 00:03:18 -0500

    fixed serveral issue

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
 
         <h1 className='text-4xl border-b-4 mb-5 w-[140px] font-bold'>Contact</h1>
         <p className='pb-5'>If you want discuss more in detail ,Please contact me.</p>
-        <p className='py-2'><span className='font-bold'>Email: yeissondanielmarinsuarez24@gmail.com</span> </p >
+        <p className='py-2'><span className='font-bold'>Email: javierandresangarita38563@gmail.com</span> </p >
         <p className='py-2'><span className='font-bold'>Phone: +57 568 967 0285</span> </p >
 
       </div>
